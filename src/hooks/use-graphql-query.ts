2f6143fbb8c66c8de37e30b14646ea1cad5cb338
mguleryuz <mguleryuz3@gmail.com>
2024-10-09 14:29:17 -0500

    feat: singleton graph subs

--- src/hooks/use-graphql-query.ts
+++ src/hooks/use-graphql-query.ts
@@ -1,288 +1,38 @@
 'use client'
 
 import { useInverter } from './use-inverter'
-import type {
-  FormattedGraphQLParams,
-  GQLBondingCurve,
-  GQLLinearVesting,
-  GQLStreamingPaymentProcessor,
-  GQLSwap,
-  GQLWorkflow,
-  GQLWorkflowModule,
-  GQLWorkflowModuleType,
-} from '@inverter-network/sdk'
 import { useQuery } from '@tanstack/react-query'
+
+import type {
+  GraphQLQueryArgs,
+  GraphQLQueryResult,
+} from '@inverter-network/sdk'
 import type { UseQueryOptions } from '@tanstack/react-query'
 import type { Except } from 'type-fest-4'
 
-export type UseBondingCurveQueryParams = {
-  project: Array<keyof GQLBondingCurve>
-  params?: FormattedGraphQLParams<GQLBondingCurve>
+export type UseGraphQLQueryParams<T extends GraphQLQueryArgs> = {
+  fields: T
   dependencies?: any[]
   options?: Except<
-    UseQueryOptions<Record<string, GQLBondingCurve[]> | undefined, Error>,
+    UseQueryOptions<GraphQLQueryResult<T> | undefined, Error>,
     'queryKey' | 'queryFn' | 'enabled'
   >
 }
 
-export type UseBondingCurveQueryReturnType = ReturnType<
-  typeof useBondingCurveQuery
->
+export type UseGraphQLQueryReturnType = ReturnType<typeof useGraphQLQuery>
 
-export const useBondingCurveQuery = ({
-  project,
-  params,
+export const useGraphQLQuery = <T extends GraphQLQueryArgs>({
+  fields,
   dependencies,
   options = {},
-}: UseBondingCurveQueryParams) => {
+}: UseGraphQLQueryParams<T>) => {
   const inverter = useInverter()
 
   const enabled = !!inverter.data
 
   const query = useQuery({
-    queryKey: ['bonding-curve', project, params, ...(dependencies || [])],
-    queryFn: () =>
-      inverter.data!.graphql.query.BondingCurve({
-        project,
-        params,
-      }),
-    enabled,
-    ...options,
-  })
-
-  return query
-}
-
-export type UseLinearVestingQueryParams = {
-  project: Array<keyof GQLLinearVesting>
-  params?: FormattedGraphQLParams<GQLLinearVesting>
-  dependencies?: any[]
-  options?: Except<
-    UseQueryOptions<Record<string, GQLLinearVesting[]> | undefined, Error>,
-    'queryKey' | 'queryFn' | 'enabled'
-  >
-}
-
-export type UseLinearVestingQueryReturnType = ReturnType<
-  typeof useLinearVestingQuery
->
-
-export const useLinearVestingQuery = ({
-  project,
-  params,
-  dependencies,
-  options = {},
-}: UseLinearVestingQueryParams) => {
-  const inverter = useInverter()
-
-  const enabled = !!inverter.data
-
-  const query = useQuery({
-    queryKey: ['linear-vesting', project, params, ...(dependencies || [])],
-    queryFn: () =>
-      inverter.data!.graphql.query.LinearVesting({
-        project,
-        params,
-      }),
-    enabled,
-    ...options,
-  })
-
-  return query
-}
-
-export type UseStreamingPaymentProcessorQueryParams = {
-  project: Array<keyof GQLStreamingPaymentProcessor>
-  params?: FormattedGraphQLParams<GQLStreamingPaymentProcessor>
-  dependencies?: any[]
-  options?: Except<
-    UseQueryOptions<
-      Record<string, GQLStreamingPaymentProcessor[]> | undefined,
-      Error
-    >,
-    'queryKey' | 'queryFn' | 'enabled'
-  >
-}
-
-export type UseStreamingPaymentProcessorQueryReturnType = ReturnType<
-  typeof useStreamingPaymentProcessorQuery
->
-
-export const useStreamingPaymentProcessorQuery = ({
-  project,
-  params,
-  dependencies,
-  options = {},
-}: UseStreamingPaymentProcessorQueryParams) => {
-  const inverter = useInverter()
-
-  const enabled = !!inverter.data
-
-  const query = useQuery({
-    queryKey: [
-      'streaming-payment-processor',
-      project,
-      params,
-      ...(dependencies || []),
-    ],
-    queryFn: () =>
-      inverter.data!.graphql.query.StreamingPaymentProcessor({
-        project,
-        params,
-      }),
-    enabled,
-    ...options,
-  })
-
-  return query
-}
-
-export type UseSwapQueryParams = {
-  project: Array<keyof GQLSwap>
-  params?: FormattedGraphQLParams<GQLSwap>
-  dependencies?: any[]
-  options?: Except<
-    UseQueryOptions<Record<string, GQLSwap[]> | undefined, Error>,
-    'queryKey' | 'queryFn' | 'enabled'
-  >
-}
-
-export type UseSwapQueryReturnType = ReturnType<typeof useSwapQuery>
-
-export const useSwapQuery = ({
-  project,
-  params,
-  dependencies,
-  options = {},
-}: UseSwapQueryParams) => {
-  const inverter = useInverter()
-
-  const enabled = !!inverter.data
-
-  const query = useQuery({
-    queryKey: ['swap', project, params, ...(dependencies || [])],
-    queryFn: () =>
-      inverter.data!.graphql.query.Swap({
-        project,
-        params,
-      }),
-    enabled,
-    ...options,
-  })
-
-  return query
-}
-
-export type UseWorkflowQueryParams = {
-  project: Array<keyof GQLWorkflow>
-  params?: FormattedGraphQLParams<GQLWorkflow>
-  dependencies?: any[]
-  options?: Except<
-    UseQueryOptions<Record<string, GQLWorkflow[]> | undefined, Error>,
-    'queryKey' | 'queryFn' | 'enabled'
-  >
-}
-
-export type UseWorkflowQueryReturnType = ReturnType<typeof useWorkflowQuery>
-
-export const useWorkflowQuery = ({
-  project,
-  params,
-  dependencies,
-  options = {},
-}: UseWorkflowQueryParams) => {
-  const inverter = useInverter()
-
-  const enabled = !!inverter.data
-
-  const query = useQuery({
-    queryKey: ['workflow', project, params, ...(dependencies || [])],
-    queryFn: () =>
-      inverter.data!.graphql.query.Workflow({
-        project,
-        params,
-      }),
-    enabled,
-    ...options,
-  })
-
-  return query
-}
-
-export type UseWorkflowModuleQueryParams = {
-  project: Array<keyof GQLWorkflowModule>
-  params?: FormattedGraphQLParams<GQLWorkflowModule>
-  dependencies?: any[]
-  options?: Except<
-    UseQueryOptions<Record<string, GQLWorkflowModule[]> | undefined, Error>,
-    'queryKey' | 'queryFn' | 'enabled'
-  >
-}
-
-export type UseWorkflowModuleQueryReturnType = ReturnType<
-  typeof useWorkflowModuleQuery
->
-
-export const useWorkflowModuleQuery = ({
-  project,
-  params,
-  dependencies,
-  options = {},
-}: UseWorkflowModuleQueryParams) => {
-  const inverter = useInverter()
-
-  const enabled = !!inverter.data
-
-  const query = useQuery({
-    queryKey: ['workflow-module', project, params, ...(dependencies || [])],
-    queryFn: () =>
-      inverter.data!.graphql.query.WorkflowModule({
-        project,
-        params,
-      }),
-    enabled,
-    ...options,
-  })
-
-  return query
-}
-
-export type UseWorkflowModuleTypeQueryParams = {
-  project: Array<keyof GQLWorkflowModuleType>
-  params?: FormattedGraphQLParams<GQLWorkflowModuleType>
-  dependencies?: any[]
-  options?: Except<
-    UseQueryOptions<Record<string, GQLWorkflowModuleType[]> | undefined, Error>,
-    'queryKey' | 'queryFn' | 'enabled'
-  >
-}
-
-export type UseWorkflowModuleTypeQueryReturnType = ReturnType<
-  typeof useWorkflowModuleTypeQuery
->
-
-export const useWorkflowModuleTypeQuery = ({
-  project,
-  params,
-  dependencies,
-  options = {},
-}: UseWorkflowModuleTypeQueryParams) => {
-  const inverter = useInverter()
-
-  const enabled = !!inverter.data
-
-  const query = useQuery({
-    queryKey: [
-      'workflow-module-type',
-      project,
-      params,
-      ...(dependencies || []),
-    ],
-    queryFn: () =>
-      inverter.data!.graphql.query.WorkflowModuleType({
-        project,
-        params,
-      }),
+    queryKey: ['graphql-query', fields, ...(dependencies || [])],
+    queryFn: () => inverter.data!.graphql.query(fields),
     enabled,
     ...options,
   })
