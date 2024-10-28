2a41b8a380047e1b4f2a99cd6b47ef111ef57160
mguleryuz <mguleryuz3@gmail.com>
2024-10-27 20:20:24 -0500

    chore: add classname to no-data

--- src/components/ui/no-data.tsx
+++ src/components/ui/no-data.tsx
@@ -1,23 +1,36 @@
+import { cn } from '@/utils'
 import { Database } from 'lucide-react'
 import * as React from 'react'
 
 export function NoData({
   inline = false,
   text = 'No Data',
+  className,
 }: {
   inline?: boolean
   text?: string
+  className?: string
 }) {
   if (inline)
     return (
-      <div className="in--flex in--justify-center in--items-center in--gap-3 in--m-auto">
+      <div
+        className={cn(
+          'in--flex in--justify-center in--items-center in--gap-3 in--m-auto',
+          className
+        )}
+      >
         <Database size={20} />
         <p>{text}</p>
       </div>
     )
 
   return (
-    <div className="in--flex in--flex-col md:in--flex-row in--justify-center in--items-center in--gap-3 in--p-5 in--m-auto">
+    <div
+      className={cn(
+        'in--flex in--flex-col md:in--flex-row in--justify-center in--items-center in--gap-3 in--p-5 in--m-auto',
+        className
+      )}
+    >
       <Database size={50} className="in--min-w-max" />
       <h3 className="in--text-center md:in--text-right">{text}</h3>
     </div>
