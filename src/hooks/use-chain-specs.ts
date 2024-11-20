e33a074558407bf6de9c5af7d4c6e23c906a0074
mguleryuz <mguleryuz3@gmail.com>
2024-11-20 16:59:56 +0700

    chore(release): 0.3.9-alpha.2

--- src/hooks/use-chain-specs.ts
+++ src/hooks/use-chain-specs.ts
@@ -2,27 +2,17 @@
 
 import * as React from 'react'
 import { useAccount, useChains } from 'wagmi'
+import { ChainLogo } from '@api3/logos'
 
 export const getIconSrc = (chainId?: number) => {
-  const fallback = 'https://chainlist.org/unknown-logo.png'
+  const fallback = (ChainLogo('1') as any)?.src || ChainLogo('1')
 
   if (!chainId) return fallback
 
-  const available = {
-    1: 'https://icons.llamao.fi/icons/chains/rsz_ethereum.jpg',
-    137: 'https://icons.llamao.fi/icons/chains/rsz_polygon.jpg',
-    80_002: 'https://icons.llamao.fi/icons/chains/rsz_polygon.jpg',
-    10: 'https://icons.llamao.fi/icons/chains/rsz_optimism.jpg',
-    250: 'https://icons.llamao.fi/icons/chains/rsz_fantom.jpg',
-    43114: 'https://icons.llamao.fi/icons/chains/rsz_avalanche.jpg',
-    8453: 'https://icons.llamao.fi/icons/chains/rsz_base.jpg',
-    59144: 'https://icons.llamao.fi/icons/chains/rsz_linea.jpg',
-    100: 'https://icons.llamao.fi/icons/chains/rsz_xdai.jpg',
-    42220: 'https://icons.llamao.fi/icons/chains/rsz_celo.jpg',
-    11155420: 'https://icons.llamao.fi/icons/chains/rsz_optimism.jpg',
-  } as Record<number, string>
+  const available =
+    (ChainLogo(chainId.toString()) as any)?.src || ChainLogo(chainId.toString())
 
-  if (available[chainId]) return available[chainId]
+  if (available) return available
 
   return fallback
 }
