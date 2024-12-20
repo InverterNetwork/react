caf5292e03afc214e67d156a1e6b0c126dd53c98
mguleryuz <mguleryuz3@gmail.com>
2024-12-20 15:24:27 +0000

    fix: theme provider complexity

--- src/provider/theme-provider.tsx
+++ src/provider/theme-provider.tsx
@@ -1,59 +1,71 @@
 'use client'
 
 import * as React from 'react'
-import {
-  getCssVariables,
-  getThemeFromCookie,
-  getThemeScript,
-} from '@/utils/theme'
 import type { InverterThemeConfig } from '@/types'
+
 import '../styles/global.css'
 
-/**
- * InverterProviderProps
- * @typedef {Object} InverterProviderProps
- * @property {React.ReactNode} children
- * @property {'light' | 'dark'} theme @default 'light'
- */
 export type ThemeProviderProps = {
   children: React.ReactNode
   themeConfig?: InverterThemeConfig
 }
 
-// Create a static style element that will be the same on server and client
-const StyleInjector = ({
-  cssVariables,
-  theme,
+const ThemeScript = ({ theme = 'light' }: { theme?: 'light' | 'dark' }) => {
+  React.useLayoutEffect(() => {
+    const derivedTheme =
+      document.cookie.match(/inverterTheme=(light|dark)/)?.[1] ||
+      theme ||
+      'light'
+    document.documentElement.setAttribute('data-inverter-theme', derivedTheme)
+    document.documentElement.style.colorScheme = derivedTheme
+  }, [])
+
+  return null
+}
+
+const ThemeStyles = ({
+  themeConfig,
 }: {
-  cssVariables: string
-  theme?: 'light' | 'dark'
-}) => (
-  <>
-    <style dangerouslySetInnerHTML={{ __html: cssVariables }} />
-    <script dangerouslySetInnerHTML={{ __html: getThemeScript(theme) }} />
-  </>
-)
-StyleInjector.displayName = 'StyleInjector'
+  themeConfig?: InverterThemeConfig
+}) => {
+  const parseVariables = (vars?: Record<string, string>) => {
+    return Object.entries(vars || {})
+      .map(([k, v]) => `--in--${k}: ${v};`)
+      .join('')
+  }
+
+  React.useLayoutEffect(() => {
+    const baseVars = parseVariables(themeConfig?.baseTheme || {})
+    const lightVars = parseVariables(themeConfig?.lightTheme || {})
+    const darkVars = parseVariables(themeConfig?.darkTheme || {})
+
+    const css = `
+      :root {${baseVars}}
+      [data-inverter-theme="light"] {${lightVars}}
+      [data-inverter-theme="dark"] {${darkVars}}
+    `
+
+    document.documentElement.style.cssText = css
+  }, [])
+
+  return null
+}
 
 export function ThemeProvider({ children, themeConfig }: ThemeProviderProps) {
   React.useEffect(() => {
-    const cookieTheme = getThemeFromCookie()
-    const propsTheme = themeConfig?.theme
-    const getTheme = () => {
-      if (propsTheme) return propsTheme
-      if (cookieTheme) return cookieTheme
-      return 'light'
-    }
-    // Update theme if it changes
-    document.documentElement.setAttribute('data-inverter-theme', getTheme())
-    document.cookie = `inverterTheme=${getTheme()}; path=/; max-age=31536000; SameSite=Strict`
+    const theme =
+      themeConfig?.theme ||
+      document.cookie.match(/inverterTheme=(light|dark)/)?.[1] ||
+      'light'
+    document.documentElement.setAttribute('data-inverter-theme', theme)
+    document.documentElement.style.colorScheme = theme
+    document.cookie = `inverterTheme=${theme}; path=/; max-age=31536000; SameSite=Strict`
   }, [themeConfig?.theme])
-
-  const cssVariables = getCssVariables(themeConfig)
 
   return (
     <>
-      <StyleInjector cssVariables={cssVariables} theme={themeConfig?.theme} />
+      <ThemeStyles themeConfig={themeConfig} />
+      <ThemeScript theme={themeConfig?.theme} />
       {children}
     </>
   )
