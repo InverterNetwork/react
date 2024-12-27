38955c41d3a5ce34bfa00523dfeec8d6ebf86074
mguleryuz <mguleryuz3@gmail.com>
2024-12-27 16:28:32 +0000

    fix: nodata component

--- src/components/ui/no-data.tsx
+++ src/components/ui/no-data.tsx
@@ -1,38 +1,33 @@
+import * as React from 'react'
 import { cn } from '@/utils'
 import { Database } from 'lucide-react'
-import * as React from 'react'
 
 export function NoData({
   inline = false,
-  text = 'No Data',
+  text,
   className,
+  orientation = 'vertical',
 }: {
   inline?: boolean
   text?: string
   className?: string
+  orientation?: 'vertical' | 'horizontal'
 }) {
-  if (inline)
-    return (
-      <div
-        className={cn(
-          'in--flex in--justify-center in--items-center in--gap-3 in--m-auto',
-          className
-        )}
-      >
-        <Database size={20} />
-        <p>{text}</p>
-      </div>
-    )
-
   return (
     <div
       className={cn(
-        'in--flex in--flex-col md:in--flex-row in--justify-center in--items-center in--gap-3 in--p-5 in--m-auto',
+        'in--justify-center in--items-center in--gap-3',
+        inline ? 'in--inline-flex' : 'in--flex in--flex-col ',
+        orientation === undefined && 'md:in--flex-row',
+        orientation === 'horizontal' && 'in--flex-row',
+        orientation === 'vertical' && 'in--flex-col',
         className
       )}
     >
-      <Database size={50} className="in--min-w-max" />
-      <h3 className="in--text-center md:in--text-right">{text}</h3>
+      <Database size={inline ? 20 : 50} className="in--min-w-max" />
+      {!!text && (
+        <p className={cn(inline ? '' : 'in--font-semibold')}>{text}</p>
+      )}
     </div>
   )
 }
