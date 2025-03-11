30e318b1e09f39659d5f99a7a5b474218a970382
mguleryuz <mguleryuz3@gmail.com>
2025-03-11 13:20:04 +0300

    refactor(theme): update color palette and theme configuration

--- src/types/theme.ts
+++ src/types/theme.ts
@@ -8,33 +8,54 @@ export type InverterThemeConfig = {
 export type CSSVariableValue = `${number}${string}` | string
 
 export type BaseTheme = {
+  radius?: CSSVariableValue
+  ring?: CSSVariableValue
+}
+
+export type ThemeColors = {
   primary?: CSSVariableValue
   'primary-foreground'?: CSSVariableValue
+
   accent?: CSSVariableValue
   'accent-foreground'?: CSSVariableValue
+
+  background?: CSSVariableValue
+  'background-2'?: CSSVariableValue
+  foreground?: CSSVariableValue
+
+  card?: CSSVariableValue
+  'card-foreground'?: CSSVariableValue
+
+  popover?: CSSVariableValue
+  'popover-foreground'?: CSSVariableValue
+
+  secondary?: CSSVariableValue
+  'secondary-foreground'?: CSSVariableValue
+
+  muted?: CSSVariableValue
+  'muted-foreground'?: CSSVariableValue
+
   destructive?: CSSVariableValue
   'destructive-foreground'?: CSSVariableValue
-  radius?: CSSVariableValue
+
+  warning?: CSSVariableValue
+  'warning-foreground'?: CSSVariableValue
+
+  success?: CSSVariableValue
+  'success-foreground'?: CSSVariableValue
+
+  error?: CSSVariableValue
+  'error-foreground'?: CSSVariableValue
+
+  info?: CSSVariableValue
+  'info-foreground'?: CSSVariableValue
 
   'chart-1'?: CSSVariableValue
   'chart-2'?: CSSVariableValue
   'chart-3'?: CSSVariableValue
   'chart-4'?: CSSVariableValue
   'chart-5'?: CSSVariableValue
-}
 
-export type ThemeColors = {
-  background?: CSSVariableValue
-  foreground?: CSSVariableValue
-  card?: CSSVariableValue
-  'card-foreground'?: CSSVariableValue
-  popover?: CSSVariableValue
-  'popover-foreground'?: CSSVariableValue
-  secondary?: CSSVariableValue
-  'secondary-foreground'?: CSSVariableValue
-  muted?: CSSVariableValue
-  'muted-foreground'?: CSSVariableValue
   border?: CSSVariableValue
   input?: CSSVariableValue
-  ring?: CSSVariableValue
 }
