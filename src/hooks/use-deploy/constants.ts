e4743b1a353869f5f16e72cbe34e4e3ac03f2a7f
mguleryuz <mguleryuz3@gmail.com>
2025-04-24 16:14:50 +0300

    chore(release): 0.4.8

--- src/hooks/use-deploy/constants.ts
+++ src/hooks/use-deploy/constants.ts
@@ -9,4 +9,12 @@ export const initialStates = {
     decimals: 18,
     maxSupply: GET_HUMAN_READABLE_UINT_MAX_SUPPLY(18),
   },
+  ERC20IssuanceUpgradeable_v1: {
+    decimals: 18,
+    maxSupply: GET_HUMAN_READABLE_UINT_MAX_SUPPLY(18),
+  },
+  ERC20IssuanceUpgradeable_Blacklist_v1: {
+    decimals: 18,
+    maxSupply: GET_HUMAN_READABLE_UINT_MAX_SUPPLY(18),
+  },
 } as const
